274f8ab9f610c287fd9cef07b820e31e0b3bfc6b
sayaka yonemasu <sayakayonemasu@fico.com>
2021-01-25 15:24:40 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+		  HI THERE!
+		  BYE THERE!
+		  HEY THERE!!
+		  COLD TODAY.
         </p>
         <a
           className="App-link"
